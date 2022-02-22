1d805dd1906a618217cde6473095b8bda3a5064f
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-02-21 20:56:16 -0500

    re-enable lint rules

--- src/Utilities/Eventlisteners.ts
+++ src/Utilities/Eventlisteners.ts
@@ -1,13 +1,19 @@
 import { player } from "../Game";
 import { reset } from "../Main/Reset/Refresh";
-import { hideStuff } from "./UpdateHTML"
+import { hideStuff } from "./UpdateHTML";
 
 export const generateEventHandlers = () => {
     document.getElementById("main-tab-nav").addEventListener('click', () => hideStuff("Main"));
     document.getElementById("upgrade-tab-nav").addEventListener('click', () => hideStuff("Upgrades"));
 
-    document.getElementById("buy-coin-bar-speed").addEventListener('click', (event:MouseEvent) => player.coinUpgrades.barSpeed.purchaseLevels(1, event));
-    document.getElementById("buy-coin-bar-momentum").addEventListener('click', (event:MouseEvent) => player.coinUpgrades.barMomentum.purchaseLevels(1, event));
+    document.getElementById("buy-coin-bar-speed").addEventListener(
+        'click',
+        (event) => player.coinUpgrades.barSpeed.purchaseLevels(1, event)
+    );
+    document.getElementById("buy-coin-bar-momentum").addEventListener(
+        'click',
+        (event) => player.coinUpgrades.barMomentum.purchaseLevels(1, event)
+    );
     document.getElementById("buy-reset").addEventListener('click', () => reset('Refresh'));
 
 }
