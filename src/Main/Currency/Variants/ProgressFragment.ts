1f8fa0dd72d610e990d9f38064b8027cf926a189
Kevin Bunn <Pseudonian@gmail.com>
2022-02-26 02:50:18 -0500

    Merge

--- src/Main/Currency/Variants/ProgressFragment.ts
+++ src/Main/Currency/Variants/ProgressFragment.ts
@@ -24,9 +24,9 @@ export class ProgressFragment extends Currency {
         if (player !== undefined)
             level = player.barLevel
 
-        let baseAmount = 25;
-        baseAmount *= Math.pow(1.04, level);
-        baseAmount *= Math.pow(1.05, Math.floor(level / 5));
+        let baseAmount = 70;
+        baseAmount *= Math.pow(1.07, level);
+        baseAmount *= Math.pow(1.07, Math.floor(level / 5));
 
         return Math.floor(baseAmount);
     }
