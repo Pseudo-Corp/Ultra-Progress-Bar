8f3752879817d96cd9891a4283936b59bfb65a31
Kevin Bunn <Pseudonian@gmail.com>
2022-03-08 06:04:05 -0500

    0.0.9: one more upgrade, talents EXP now visible, bug fix

---
+++ src/Main/Transformations/CoinUpgrades/BarResonance.ts
@@ -0,0 +1,15 @@
+import type { Player } from '../../../types/player';
+import { getProperty } from 'dot-prop';
+import { CoinBarResonance, coinUpgradeCosts } from '../../Upgrades/Variants/Coin';
+
+const path = 'coinUpgrades.barResonance' as const;
+
+export const transform = (data: Partial<Player>, player: Player) => {
+    const value = getProperty(data, path) ?? getProperty(player, path);
+
+    return new CoinBarResonance(
+        value?.level ?? 0,
+        coinUpgradeCosts.barResonance,
+        player
+    )
+}
