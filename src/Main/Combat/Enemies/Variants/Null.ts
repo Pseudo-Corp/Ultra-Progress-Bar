951048ee0ba37dbb17aa1f20dae3a25473cb7362
Pseudonian <Pseudonian@gmail.com>
2022-07-08 02:23:43 -0400

    feature: Combat more thoroughly implemented into game for real

---
+++ src/Main/Combat/Enemies/Variants/Null.ts
@@ -0,0 +1,28 @@
+import { Player } from '../../../../types/player';
+import { format } from '../../../../Utilities/Format';
+import { updateElementById } from '../../../../Utilities/Render';
+import { enemyStats } from '../../Stats/Stats';
+import { combatHTMLReasons } from '../../types';
+import { Enemy, EnemyTypes } from '../Enemy';
+
+export class NullEnemy extends Enemy {
+    enemyType:EnemyTypes = 'Null'
+
+    constructor(stats: enemyStats, attackRate: number, player: Player) {
+        super(stats, attackRate, player);
+        this.variantSpecificHTML('Initialize')
+    }
+
+    async enemyAI(): Promise<void> {
+        await this.doNothing();
+    }
+
+    variantSpecificHTML(reason: combatHTMLReasons): void {
+        if (reason === 'Initialize') {
+            updateElementById(
+                'enemyName',
+                { textContent: `${this.enemyType} Dummy Lv${format(this.level)}` }
+            )
+        }
+    }
+}
---
+++ src/Main/Transformations/Combat/Fighter.ts
@@ -0,0 +1,15 @@
+import { getProperty } from 'dot-prop';
+import { Player } from '../../../types/player';
+import { baseAttackRate, baseFighterStats, PlayerFighter } from '../../Combat/Player/Fighter';
+
+const path = 'fighter' as const;
+
+export const transform = (data: Partial<Player>, player: Player) => {
+    const value = getProperty(data, path) ?? getProperty(player, path);
+
+    return new PlayerFighter(
+        value?.baseStats ?? baseFighterStats,
+        value?.attackRate ?? baseAttackRate,
+        player
+    );
+}
